d73cbedc17afd3934e0c3c494a9b1084a1c6ede5
Alimo16 <alimohamedmoniem@gmail.com>
2023-03-16 05:15:56 -0700

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- *
- * Return Always 0 (Success)
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a newline.
+ * Return Always 0 (Success).
  */
 int main(void)
 {
