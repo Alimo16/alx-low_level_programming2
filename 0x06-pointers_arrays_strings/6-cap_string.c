77a40aab35538e5245226bb77d2a818dc3f8bd63
Alimo16 <alimohamedmoniem@gmail.com>
2023-03-30 07:36:24 -0700

    point

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,12 @@
 char *cap_string(char *str)
 {
 	int index = 0;
+
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
